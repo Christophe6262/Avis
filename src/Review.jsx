d7fe3ea338932e2b747f79d93c9fc265e4ef2e13
Chris D <131358614+Christophe6262@users.noreply.github.com>
2023-07-05 17:09:32 +0200

    my first commit

---
+++ src/Review.jsx
@@ -0,0 +1,41 @@
+/* eslint-disable react/prop-types */
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { BiSolidQuoteRight } from "react-icons/Bi";
+const Review = ({
+  person,
+  handleNextView,
+  handlePreview,
+  handleRandomView,
+}) => {
+  const { name, job, image, id, text } = person;
+
+  return (
+    <article key={id} className="review">
+      <div className="img-container">
+        <img className="person-img" src={image} alt={name} />
+        <span className="quote-icon">
+          <BiSolidQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button className="prev-btn" onClick={handlePreview}>
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn" onClick={handleNextView}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button
+        className="btn-hipster btn
+        "
+        onClick={handleRandomView}
+      >
+        Surprise Me
+      </button>
+    </article>
+  );
+};
+export default Review;
